893b973331b7f130cac3b714f57d1fa601f3a449
R B <86495244+ryzenRB@users.noreply.github.com>
2022-09-12 17:39:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
 #include <iostream>
 using namespace std;
 
-void solve();
-
 int main() {
-	
-	int n,p1,p2,winner,sum1=0,sum2=0,difference=0;
+
+	int n,i,z,sum1=0,sum2=0,diff=0;
 	cin>>n;
 	
-	int a[n];
-	int b[n];
-	for(int i=1;i<=n;i++){
+	int a[n],b[n];
+	
+	for(i=0;i<n;i++)
+	{
 	    cin>>a[i];
-	    sum1 = sum1+a[i];
+	    sum1=sum1+a[i];
 	    
 	    cin>>b[i];
-	    sum2 = sum2+b[i];
-	    
-	    if(sum1>sum2 && difference<(sum1-sum2)){
-	        winner=1;
-	        difference=sum1-sum2;
+	    sum2=sum2+b[i];
+	
+	   
+	 if(sum1>sum2 && diff<(sum1-sum2))
+	    {
+	        z=1;
+	        diff=(sum1-sum2);
 	    }
-	    else if(sum2>sum1 && difference<(sum1-sum2)){
-	        winner = 2;
-	        difference=sum2-sum1;
+	    else if(sum1<sum2 && diff<(sum2-sum1))
+	    {
+	        z=2;
+	        diff=(sum2-sum1);
 	    }
 	}
-	
-    cout << winner << " " << difference << endl;
+	 
+	cout<<z<<" "<<diff<<endl;
 	return 0;
 }
